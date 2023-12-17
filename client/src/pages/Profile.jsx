c392764bd1d9a86929fa425c476690e9ad3fef66
Sai <maragonirohansai@gmail.com>
2023-12-17 18:09:47 +0530

    profile image uploading functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,14 +1,76 @@
-import React from 'react'
+import { useState ,useRef,useEffect } from 'react';
+import {getDownloadURL, getStorage ,ref, uploadBytesResumable} from 'firebase/storage';
+import {app} from '../firebase'
 import { useSelector } from 'react-redux'
 export default function Profile() {
   const {currentUser}= useSelector((state)=>state.user);
+  const fileRef=useRef(null);
+  const [file,setFile]=useState(undefined);
+  const [filePerc,setFilePerc]=useState(0);
+  const [fileUploadError,setFileUploadError]=useState(false);
+  const [formData,setFormData]=useState({});
+
+  useEffect(()=>{
+    if(file){
+      handleFileUpload(file);
+    }
+  },[file]);
+
+
+  const handleFileUpload=(file)=>{
+    const storage = getStorage(app);
+    const fileName =new Date().getTime() + file.name;
+    const storageRef=ref(storage,fileName);
+    const uploadTask = uploadBytesResumable(storageRef,file);
+
+    uploadTask.on('state_changed',
+      (snapshot)=>{
+        const progress= (snapshot.bytesTransferred/snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress))
+      },
+    (error)=>{
+      setFileUploadError(true);
+    },
+    ()=>{
+      getDownloadURL(uploadTask.snapshot.ref).then(
+        (downloadURL) =>{
+          setFormData({ ...formData , avatar: downloadURL});
+        }
+      );
+    }
+    );
+  };
+
   return (
-    <div className='p-3 max--w-lg mx-auto'>
+    <div className='p-3 max-w-lg mx-auto'>
       <h1 className="text-3xl font-semibold text-center my-7">
         Profile
       </h1>
       <form className='flex flex-col gap-4'>
-        <img className="rounded-full h-24 w-24  object-cover cursor-pointer self-center mt-2" src={currentUser.avatar} alt='profile'/>
+        <input hidden 
+        onChange={(e)=>setFile(e.target.files[0])}
+        type='file' 
+        ref={fileRef} 
+        accept='image/*'/>
+        <img onClick={()=>fileRef.current.click()} 
+        src={formData.avatar || currentUser.avatar} alt='profile' 
+        className="rounded-full h-24 w-24  object-cover cursor-pointer self-center mt-2" />
+        <p className='text-sm self-center'>
+          { 
+          fileUploadError ? (
+            <span className='text-red-700'>Upload Size Error. Image nust be less than 2mb</span>
+          ): filePerc > 0 && filePerc < 100 ? (
+            <span className='text-purple-700  '>
+              {'Uploading '}{filePerc}{'%'}
+            </span>)
+            : filePerc === 100 ?( 
+                <span className='text-green-700'>
+                  Successfully Uploaded !
+                </span>
+            ):
+            ( '' )
+          }
+        </p>
         <input className='border p-3 rounded-lg' type='text' placeholder='username' id='username'/>
         <input className='border p-3 rounded-lg' type='email' placeholder='email' id='email'/>
         <input className='border p-3 rounded-lg' type='password' placeholder='password' id='password'/>
@@ -25,3 +87,12 @@ export default function Profile() {
     </div>
   )
 }
+//Rules to be changed to make read and write in github
+/*
+      allow read;
+      allow write: if 
+      request.resource.size <2*1024*1024 &&
+      request.resource.contentType.matches('image/.*');
+*/
+
+//para tag is somewhat problematic
