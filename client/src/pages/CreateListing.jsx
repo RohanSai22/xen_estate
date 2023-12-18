223afb8c95822de5fb6e5903cf119167a24634bb
Sai <maragonirohansai@gmail.com>
2023-12-18 20:22:56 +0530

    Completed listing images uploading functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,103 @@
-import React from 'react'
+import { getDownloadURL, getStorage , ref ,uploadBytesResumable} from 'firebase/storage';
+import { app } from '../firebase.js'; 
+import React, { useState } from 'react'
 
 export default function CreateListing() {
+    const [files,setFiles]=useState([]);
+    const [formData,setFormData]= useState({
+        imageUrls:[],
+    });
+
+    const [imageUploadError, setImageUploadError] = useState(false);
+
+    const [uploading,setUploading]= useState(false);
+
+    console.log(formData);
+    const handleImageSubmit = (e)=>{
+        if(files.length>0 && files.length + formData.imageUrls.length < 7){
+            setUploading(true);
+            setImageUploadError(false);
+            const promises=[];//we have more than 1 async behaviour so we need to wait for all of them and one by one image must be uploaded to storage
+
+            for( let i=0;i<files.length;i++){
+                promises.push(storeImage(files[i]));
+            }
+        Promise.all(promises).then((urls)=>{
+            setFormData({ ...formData, 
+                imageUrls: formData.imageUrls.concat(urls),
+             });
+        setImageUploadError(false);
+        setUploading(false);
+        }).catch((error)=>{
+            setImageUploadError("Image Upload Failed (2 mb max for each image)");
+            setUploading(false);
+        });
+        }
+        else{
+            setImageUploadError("You can upload only 6 images per listing");
+            setUploading(false);
+        }
+    };
+
+   /* const handleFileUpload=(file)=>{
+        const storage = getStorage(app);
+        const fileName =new Date().getTime() + file.name;
+        const storageRef=ref(storage,fileName);
+        const uploadTask = uploadBytesResumable(storageRef,file);
+    
+        uploadTask.on('state_changed',
+          (snapshot)=>{
+            const progress= (snapshot.bytesTransferred/snapshot.totalBytes) * 100;
+            setFilePerc(Math.round(progress))
+          },
+        (error)=>{
+          setFileUploadError(true);
+        },
+        ()=>{
+          getDownloadURL(uploadTask.snapshot.ref).then(
+            (downloadURL) =>{
+              setFormData({ ...formData , avatar: downloadURL});
+            }
+          );
+        }
+        );
+      };*/
+
+      
+//storeImage is same as upload image in profile
+    const storeImage= async (file)=>{
+        return new Promise((resolve,reject)=>{
+            const storage= getStorage(app);//to get storage of firebase
+            const fileName= new Date().getTime() + file.name;
+            const storageRef=ref(storage,fileName);
+            const uploadTask= uploadBytesResumable(storageRef,file);
+
+            uploadTask.on(
+                "state-changed",
+                //her we can add progress of uploading images we can add it or not(Wrong ) we need to add snapshot even if we dont want to show progress
+                (snapshot)=>{
+                    const progress=(snapshot.bytesTransferred/snapshot.totalBytes)*100;
+                    console.log(`Upload progess is ${progress}%`);
+                },
+                (error)=>{
+                    reject(error);
+                },
+                ()=>{
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
+                        resolve(downloadURL);
+                    });
+                },
+            );
+        });
+    };
+
+    const handleRemoveImage =(index) =>{
+        setFormData({
+            ...formData,
+            imageUrls:formData.imageUrls.filter((_,i) => i!== index),
+        })
+    };
+
   return (
     <main className='p-3 max-w-4xl mx-auto'>
         <h1 className='text-center text-3xl my-7 font-bold'>Create a Listing</h1>
@@ -72,14 +169,69 @@ export default function CreateListing() {
                     </span>
                 </p>
                 <div className='flex gap-4'>
-                    <input className='p-3 border border-grey-300 rounded w-full' type='file' id ='images' accept='image/*' multiple/>
-                    <button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>Upload</button>
+                    <input onChange={(e)=>setFiles(e.target.files)} className='p-3 border border-grey-300 rounded w-full' type='file' id ='images' accept='image/*' multiple/>
+                    <button disabled={uploading} type='button' onClick={handleImageSubmit} className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80' >{uploading? 'Uploading...' :'Upload'}</button>
                 </div>
-            <button className=' p-3 bg-orange-500 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity:80 ' >Create Listing</button>
+            <p className='text-red-700 text-sm'>{imageUploadError && imageUploadError}</p>
+                {
+                    formData.imageUrls.length > 0 && formData.imageUrls.map( (url,idx) => (
+                    <div key={url} className='flex justify-between p-3 border items-center'>
+                    <img src={url} alt='listing image' className='w-20 h-20 object-contain rounded-lg'/>
+                    <button type='button' onClick={()=>handleRemoveImage(idx)} className='p-3 rounde-lg uppercase hover:opacity-70 text-red-500'>Delete</button>
+                    </div>
+                ))
+                }
+            <button className=' p-3 bg-cyan-600 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity:80 ' >Create Listing</button>
             </div>
         </form>
     </main>
     
   )
-}
+};
+//since button upload is inside a form we need to set its type to button by specifying  but if we specify that whole form will be submitted so we dont do that
 //if we want both divs to occupy same space when screen size is increased or decreased then we use flex as flex-1 flex-1 for every div which we need for flex
+
+                                            //This is the template to upload files from local storage to online storage of firebase 
+ /* const handleFileUpload=(file)=>{
+        const storage = getStorage(app);
+        const fileName =new Date().getTime() + file.name;
+        const storageRef=ref(storage,fileName);
+        const uploadTask = uploadBytesResumable(storageRef,file);
+    
+        uploadTask.on('state_changed',
+          (snapshot)=>{
+            const progress= (snapshot.bytesTransferred/snapshot.totalBytes) * 100;
+            setFilePerc(Math.round(progress))
+          },
+        (error)=>{
+          setFileUploadError(true);
+        },
+        ()=>{
+          getDownloadURL(uploadTask.snapshot.ref).then(
+            (downloadURL) =>{
+              setFormData({ ...formData , avatar: downloadURL});
+            }
+          );
+        }
+        );
+      };*/
+//             Important : we need to add snapshot even if we dont want to show any progress
+
+
+
+/*
+                                How  to use the Map function:VVI
+                {
+                    formData.imageUrls.length > 0 && formData.imageUrls.map( (url) => (
+                    <img src={url} alt='listing image' className='w-40 h-40 object-cover rounded-lg'/>
+                ))
+                }
+
+
+
+
+
+
+
+
+*/
