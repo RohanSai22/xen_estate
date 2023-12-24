2343b8aeef0e535f92c5e1fa4ef041b5fcf2658f
Sai <maragonirohansai@gmail.com>
2023-12-24 12:05:31 +0530

    All functionalities are ok but the error messages are creating a problem even insomnia is problematic somewhat

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req,res,next) =>{
     try{
@@ -7,5 +8,24 @@ export const createListing = async (req,res,next) =>{
     }
     catch(error){
         next(error);//middleware next is used to handle the error
+    }
+};
+
+export const deleteListing = async (req,res,next)=>{
+    const listing = await Listing.findById(req.params.id);
+    if(!listing){
+        return next(errorHandler(404,"Listing not found !"));
+    }
+    if(req.user.id !== listing.userRef ){
+            return next(errorHandler(401,"You can only delete your listing"));
+    }
+    else{
+        try{
+            await Listing.findByIdAndDelete(req.params.id);
+            res.status(200).json('Listing has been deleted !');
+        }
+        catch(error){
+            next(error);
+        }
     }
 };
